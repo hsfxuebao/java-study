378713eac71689bfd40c2717746840f85167a464
haoshaofei <haoshaofei@guazi.com>
2020-03-15 21:37:07 +0800

    栈队列算法

---
+++ src/main/java/common/hash/N_02_并查集.java
@@ -0,0 +1,101 @@
+package common.hash;
+
+import com.google.common.base.Objects;
+import com.google.common.collect.Maps;
+import com.sun.xml.internal.xsom.XSWildcard;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author haoshaofei
+ * @date 2020/03/15
+ * <p>
+ * 合并和查找集合：https://juejin.im/post/5deb99756fb9a01600533063
+ */
+public class N_02_并查集 {
+
+	public static class Node {
+
+		//whatever you like
+	}
+
+	public static class UnionFindSet {
+
+		public Map<Node, Node> fatherMap;
+		public Map<Node, Integer> sizeMap;
+
+		public UnionFindSet() {
+			this.fatherMap = Maps.newHashMap();
+			this.sizeMap = Maps.newHashMap();
+		}
+
+		//初始化所有的数据
+		public void makeSets(List<Node> nodes) {
+
+			fatherMap.clear();
+			sizeMap.clear();
+			for (Node node : nodes) {
+				this.fatherMap.put(node, node);
+				this.sizeMap.put(node, 1);
+			}
+		}
+
+		/**
+		 * 查找一个结点的头结点并设置该节点的头结点
+		 *
+		 * @param node
+		 * @return
+		 */
+		public Node findHead(Node node) {
+			Node fatherNode = fatherMap.get(node);
+			if (Objects.equal(fatherNode, node)) {
+				fatherNode = findHead(fatherNode);
+			}
+			fatherMap.put(node, fatherNode);
+			return fatherNode;
+		}
+
+		/**
+		 * 判断两个结点是否在同一个集合中
+		 *
+		 * @param a
+		 * @param b
+		 * @return
+		 */
+		public boolean isSameSet(Node a, Node b) {
+			return Objects.equal(findHead(a), findHead(b));
+		}
+
+
+		/**
+		 * 合并两个集合
+		 */
+		public void union(Node a, Node b) {
+			if (a == null || b == null) {
+				return;
+			}
+			Node aHead = findHead(a);
+			Node bHead = findHead(b);
+
+			if (!Objects.equal(aHead, bHead)) {
+
+				Integer aSize = sizeMap.get(a);
+				Integer bSize = sizeMap.get(b);
+
+				if (aSize >= bSize) {
+					fatherMap.put(bHead, aHead);
+					sizeMap.put(aHead, aSize + bSize);
+				} else {
+					fatherMap.put(aHead, bHead);
+					sizeMap.put(bHead, aSize + bSize);
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		//
+	}
+}
---
+++ src/main/java/common/stackque/N_03_栈中最小元素.java
@@ -0,0 +1,106 @@
+package common.stackque;
+
+/**
+ * @author haoshaofei
+ * @date 2020/03/15
+ */
+
+import com.google.common.base.Objects;
+
+import java.util.Stack;
+
+/**
+ * 实现一个特殊的栈，在实现栈的基本功能的基础上，再实现返
+ * 回栈中最小元素的操作。
+ * 【要求】 1.pop、push、getMin操作的时间复杂度都是O(1)。 2.设计的栈类型可以使用现成的栈结构。
+ */
+public class N_03_栈中最小元素 {
+
+
+	/**
+	 * 最小栈和数据栈的数量是一样的
+	 */
+	public static class MinStack1 {
+
+		private Stack<Integer> stackData;
+		private Stack<Integer> stackMin;
+
+		public MinStack1() {
+			stackData = new Stack<Integer>();
+			stackMin = new Stack<Integer>();
+		}
+
+		public void push(int value) {
+			if (stackMin.isEmpty()) {
+				stackMin.push(value);
+			} else if (value < this.getMin()) {
+				stackMin.push(value);
+			} else {
+				int number = stackMin.peek();
+				stackMin.push(number);
+			}
+
+			stackData.push(value);
+		}
+
+		public Integer pop() {
+			if (stackData.isEmpty()) {
+				throw new RuntimeException("栈中元素是空的");
+			}
+			stackMin.pop();
+			return stackData.pop();
+		}
+
+		public Integer getMin() {
+			if (stackMin.isEmpty()) {
+				throw new RuntimeException("栈中的元素是空的");
+			}
+			return stackMin.peek();
+		}
+
+	}
+
+
+	public static class MinStack2 {
+
+		private Stack<Integer> stackData;
+		private Stack<Integer> stackMin;
+
+		public MinStack2() {
+			stackData = new Stack<Integer>();
+			stackMin = new Stack<Integer>();
+		}
+
+		public void push(int value) {
+
+			if (stackMin.isEmpty()) {
+				stackMin.push(value);
+			} else if (value <= getMin()) {
+				stackMin.push(value);
+			}
+			stackData.push(value);
+		}
+
+		public Integer pop() {
+			if (stackData.isEmpty()) {
+				throw new RuntimeException("stack is empty");
+			}
+
+			Integer number = stackData.pop();
+			if (Objects.equal(number, getMin())) {
+				stackMin.pop();
+			}
+
+			return number;
+		}
+
+		public Integer getMin() {
+			if (stackMin.isEmpty()) {
+				throw new RuntimeException("stack is empty");
+			}
+			return stackMin.peek();
+		}
+
+	}
+
+}
