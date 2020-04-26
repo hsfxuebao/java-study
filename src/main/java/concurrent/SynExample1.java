19f44abc62ba8fa182c9eb1699a9b1db58966ec6
haoshaofei <haoshaofei@guazi.com>
2020-04-26 17:23:19 +0800

    增加并发代码示例

---
+++ src/main/java/concurrent/SynExample1.java
@@ -0,0 +1,36 @@
+package concurrent;
+
+public class Example1 {
+    //1.创建共享变量
+    private static boolean flag = true;
+
+    public static void main(String[] args) throws Exception {
+        //2.t1空循环，如果flag为true,不退出
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    //新增的打印语句
+//                    System.out.println(flag);
+                    if(!flag){
+                        System.out.println("进入if");
+                        break;
+                    }
+                }
+            }
+        });
+        t1.start();
+
+        Thread.sleep(2000L);
+        //2.t2修改flag为false
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                flag = false;
+                System.out.println("修改了");
+            }
+        });
+
+        t2.start();
+    }
+}
---
+++ src/main/java/concurrent/SynMark.java
@@ -0,0 +1,8 @@
+package concurrent;
+
+/**
+ * @author haoshaofei
+ * @date 2020/04/22
+ */
+public class SynMark {
+}
